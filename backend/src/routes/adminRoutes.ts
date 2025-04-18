789a17cf07860abcae1bccc864a7c6b50186457a
subhana Thasni <subhanathasni@gmail.com>
2025-04-18 13:04:48 +0530

    connection url

--- backend/src/routes/adminRoutes.ts
+++ backend/src/routes/adminRoutes.ts
@@ -1,7 +1,6 @@
 import { Router } from "express";
 import AuthController from "../controllers/AdminController";
 import SpecialityController from "../controllers/specialityController";
-import specialityController from "../controllers/specialityController";
 import { verifyToken } from "../middleware/authMiddleware";
 import {validate} from "../middleware/validationMiddleware"
 
@@ -9,7 +8,7 @@ const router = Router();
 
 router.post("/login", validate(),AuthController.login);
 router.get("/specialities",SpecialityController.getAllSpecialities);
-router.get("/specialities/status",specialityController.specialityStatus);
+router.get("/specialities/status",SpecialityController.specialityStatus);
 router.post("/specialities", verifyToken(["admin"]),SpecialityController.addSpeciality);
 router.put("/specialities/:id/toggle",verifyToken(["admin"]), SpecialityController.toggleSpecialityStatus);
 export default router;
