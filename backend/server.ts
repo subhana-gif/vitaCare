93f2bf97b028b881404d3e30a155f6b770cf27a1
subhana Thasni <subhanathasni@gmail.com>
2025-04-18 10:59:38 +0530

    socket

--- backend/server.ts
+++ backend/server.ts
@@ -30,7 +30,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173", 
+    origin: "https://vitacare.life", 
     credentials: true,
     methods: "GET, POST, PUT, DELETE, OPTIONS",
     allowedHeaders: "Content-Type, Authorization",
