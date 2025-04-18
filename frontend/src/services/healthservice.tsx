aecce5a40e4728986f199b76ba8903ea4d4a796f
subhana Thasni <subhanathasni@gmail.com>
2025-04-18 16:31:04 +0530

    health article

--- frontend/src/services/healthservice.tsx
+++ frontend/src/services/healthservice.tsx
@@ -1,19 +1,12 @@
 import axios from "axios";
 
-const API_KEY = "d187e4cf008845abbd4a4f18733ecd7b"; // Replace with your API key
+const API_KEY = "d187e4cf008845abbd4a4f18733ecd7b";
 const BASE_URL = "https://newsapi.org/v2/everything";
 
 export const fetchHealthArticles = async () => {
   try {
-    const response = await axios.get(`${BASE_URL}`, {
-      params: {
-        q: "health",
-        apiKey: API_KEY,
-        pageSize: 6, // Limit the number of articles
-        language: "en",
-      },
-    });
-    return response.data.articles;
+    const response = await axios.get(`https://gnews.io/api/v4/search?q=health&lang=en&token=${API_KEY}`);
+      return response.data.articles;
   } catch (error) {
     console.error("Error fetching health articles:", error);
     return [];
