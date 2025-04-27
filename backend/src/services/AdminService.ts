349c8bd437d44c768ffbc26ec6008177f05043cb
subhana Thasni <subhanathasni@gmail.com>
2025-04-28 05:09:38 +0530

    vercel

--- backend/src/services/AdminService.ts
+++ backend/src/services/AdminService.ts
@@ -1,20 +1,20 @@
-import { IAuthService } from "../interfaces/IAuthService";
-import { ITokenService } from "../interfaces/ITokenService";
-import TokenService from "./tokenService";
+  import { IAuthService } from "../interfaces/IAuthService";
+  import { ITokenService } from "../interfaces/ITokenService";
+  import TokenService from "./tokenService";
 
-class AuthService implements IAuthService {
-  constructor(private tokenService: ITokenService) {}
+  class AuthService implements IAuthService {
+    constructor(private tokenService: ITokenService) {}
 
-  async login(email: string, password: string): Promise<string> {
-    const ADMIN_EMAIL = process.env.ADMIN_EMAIL || "admin@gmail.com";
-    const ADMIN_PASSWORD = process.env.ADMIN_PASSWORD || "admin123";
+    async login(email: string, password: string): Promise<string> {
+      const ADMIN_EMAIL = process.env.ADMIN_EMAIL || "admin@gmail.com";
+      const ADMIN_PASSWORD = process.env.ADMIN_PASSWORD || "admin123";
 
-    if (email.trim() !== ADMIN_EMAIL.trim() || password.trim() !== ADMIN_PASSWORD.trim()) {
-      throw new Error("Invalid credentials");
+      if (email.trim() !== ADMIN_EMAIL.trim() || password.trim() !== ADMIN_PASSWORD.trim()) {
+        throw new Error("Invalid credentials");
+      }
+
+      return this.tokenService.generateToken({ email: ADMIN_EMAIL, role: "admin", id: "admin123" });
     }
-
-    return this.tokenService.generateToken({ email: ADMIN_EMAIL, role: "admin", id: "admin123" });
   }
-}
 
-export default new AuthService(TokenService);
+  export default new AuthService(TokenService);
